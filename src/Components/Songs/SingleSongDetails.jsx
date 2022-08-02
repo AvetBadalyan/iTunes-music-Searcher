872f2c4769555ff30593828de0cd2697f4b5b2ff
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-02 14:09:53 +0400

    feat: add song info to the detail page dynamically

--- src/Components/Songs/SingleSongDetails.jsx
+++ src/Components/Songs/SingleSongDetails.jsx
@@ -18,13 +18,36 @@ export default function SingleSongDetails() {
     fetchMusic();
   }, [trackId]);
 
+  const millisToMinutesAndSeconds = (millis) => {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes} minutes ${seconds < 10 ? "0" : ""}${seconds} seconds`;
+  };
+
+  const duration = millisToMinutesAndSeconds(song.trackTimeMillis);
+
   return (
     <div className="song-details">
       <div className="song-details-content">
         <h1>iTunes Music Searcher</h1>
         <Link to="/">Go Back</Link>
-        <div className="singer-image">
-          <img src={song.artworkUrl100} alt="artist-image" />
+        <div className="song-image-and-info">
+          <div className="singer-image">
+            <img src={song.artworkUrl100} alt="artist-image" />
+          </div>
+          <div className="song-infos">
+            <div className="song-info">track Price: {song.trackPrice} $</div>
+            <div className="song-info"> release Date: {song.releaseDate}</div>
+            <div className="song-info">duration: {duration}</div>
+            <div className="song-info">country: {song.country}</div>
+            <div className="song-info">currency: {song.currency}</div>
+            <div className="song-info">
+              primary Genre: {song.primaryGenreName}
+            </div>
+            <div className="song-info">
+              <p>Collection: {song.collectionName}</p>
+            </div>
+          </div>
         </div>
         <div className="song-text">
           {song.artistName} - {song.trackName}
@@ -32,6 +55,22 @@ export default function SingleSongDetails() {
         <div>
           <p>Preview:</p>
           <audio src={song.previewUrl} controls />
+        </div>
+
+        <div>
+          <a target="_blank" href={song.artistViewUrl}>
+            Check the Artist's page on Apple music'
+          </a>
+        </div>
+        <div>
+          <a target="_blank" href={song.collectionViewUrl}>
+            Check the collection on Apple music'
+          </a>
+        </div>
+        <div>
+          <a target="_blank" href={song.trackViewUrl}>
+            Check the track on Apple music'
+          </a>
         </div>
       </div>
     </div>
