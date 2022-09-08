1284a10fe8361a3db29ffa0ae9e84b2c28ad3190
AvetBadalyan <avetbadalyan@gmail.com>
2022-09-09 02:20:18 +0400

    feat: mobile UI update of main page

--- src/Components/Songs/SingleSongDiv.jsx
+++ src/Components/Songs/SingleSongDiv.jsx
@@ -3,10 +3,18 @@ import { Link } from "react-router-dom";
 import "./SingleSongDiv.css";
 
 export default function SingleSongDiv({ result }) {
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    // The viewport is less than 768 pixels wide
+    let tags = document.getElementsByClassName("single-song-details");
+    for (let i = 0; i < tags.length; i++) {
+      tags[i].target = "_self";
+    }
+  }
+
   return (
     <div className="single-song">
       <div className="single-song-img">
-        <img src={result.artworkUrl100} alt="singer"/>
+        <img src={result.artworkUrl100} alt="singer" />
       </div>
       <div className="single-song-artist-name">
         {result.artistName.length < 30
